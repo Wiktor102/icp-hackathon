9819fd5cae2401281501464917ce0ce0872f2c5e
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-12-11 20:50:03 +0100

    Customize profile menu dropdown

--- src/icp-hackathon-frontend/src/Header/Header.jsx
+++ src/icp-hackathon-frontend/src/Header/Header.jsx
@@ -1,4 +1,9 @@
-import { ConnectWallet } from "@nfid/identitykit/react";
+import {
+	ConnectWallet,
+	ConnectWalletDropdownMenu,
+	ConnectWalletDropdownMenuButton,
+	ConnectWalletDropdownMenuItems
+} from "@nfid/identitykit/react";
 import Button from "../common/Button";
 import { Link, Outlet, useLocation } from "react-router";
 
@@ -14,7 +19,10 @@ function Header() {
 					<h1>SklepX</h1>
 				</Link>
 				<div>
-					<ConnectWallet connectButtonComponent={props => <Button {...props}>Zaloguj się</Button>} />
+					<ConnectWallet
+						connectButtonComponent={props => <Button {...props}>Zaloguj się</Button>}
+						dropdownMenuComponent={ProfileDropdown}
+					/>
 					{!location.includes("cart") && (
 						<Link to="/cart" className="page-header__cart">
 							<i className="fas fa-cart-shopping"></i>
@@ -24,6 +32,31 @@ function Header() {
 			</header>
 			<Outlet />
 		</>
+	);
+}
+
+function ProfileDropdown({ connectedAccount, icpBalance, disconnect }) {
+	return (
+		<ConnectWalletDropdownMenu>
+			<ConnectWalletDropdownMenuButton connectedAccount={connectedAccount} icpBalance={icpBalance}>
+				<Button>
+					<i className="fas fa-user"></i>
+					Profil
+				</Button>
+			</ConnectWalletDropdownMenuButton>
+			<ConnectWalletDropdownMenuItems>
+				<Link to="/profile">
+					<div className="profile-dropdown-item">
+						<i className="fas fa-circle-info"></i>
+						<div className="profile-dropdown-item__label">Szczegóły</div>
+					</div>
+				</Link>
+				<div className="profile-dropdown-item" onClick={disconnect}>
+					<i className="fas fa-arrow-right-from-bracket"></i>
+					<div className="profile-dropdown-item__label">Wyloguj się</div>
+				</div>
+			</ConnectWalletDropdownMenuItems>
+		</ConnectWalletDropdownMenu>
 	);
 }
 
