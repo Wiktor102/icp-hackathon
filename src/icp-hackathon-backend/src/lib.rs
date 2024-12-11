dc00f41af17d1a38d08e86300702373158667bf7
Justyn Odyjas <60746042+Reiv21@users.noreply.github.com>
2024-12-11 17:35:31 +0100

    Backend update

--- src/icp-hackathon-backend/src/lib.rs
+++ src/icp-hackathon-backend/src/lib.rs
@@ -1,6 +1,49 @@
 use std::cell::RefCell;
 use crate::listing::Listing;
 use crate::config::Config;
+use candid::{self, CandidType, Deserialize};
+ 
+#[derive(CandidType, Deserialize, Eq, PartialEq, Debug)]
+pub struct SupportedStandard {
+    pub url: String,
+    pub name: String,
+}
+
+#[ic_cdk::update]
+fn icrc10_supported_standards() -> Vec<SupportedStandard> {
+    vec![
+        SupportedStandard {
+            url: "https://github.com/dfinity/ICRC/blob/main/ICRCs/ICRC-10/ICRC-10.md".to_string(),
+            name: "ICRC-10".to_string(),
+        },
+        SupportedStandard {
+            url: "https://github.com/dfinity/wg-identity-authentication/blob/main/topics/icrc_28_trusted_origins.md".to_string(),
+            name: "ICRC-28".to_string(),
+        },
+    ]
+}
+ 
+#[derive(Clone, Debug, CandidType, Deserialize)]
+pub struct Icrc28TrustedOriginsResponse {
+    pub trusted_origins: Vec<String>
+}
+ 
+// list every base URL that users will authenticate to your app from
+#[ic_cdk::update]
+fn icrc28_trusted_origins() -> Icrc28TrustedOriginsResponse {
+    let trusted_origins = vec![
+        String::from("https://your-frontend-canister-id.icp0.io"),
+        String::from("https://your-frontend-canister-id.raw.icp0.io"),
+        String::from("https://your-frontend-canister-id.ic0.app"),
+        String::from("https://your-frontend-canister-id.raw.ic0.app"),
+        String::from("https://your-frontend-canister-id.icp0.icp-api.io"),
+        String::from("https://your-frontend-canister-id.icp-api.io"),
+        String::from("https://yourcustomdomain.com"),
+        String::from("https://yourothercustomdomain.com")
+    ];
+ 
+    return Icrc28TrustedOriginsResponse { trusted_origins }
+}
 
 mod listing;
 mod config;
@@ -36,6 +79,9 @@ fn get_listings() -> Vec<Listing> {
 fn get_config() -> Config {
     CONFIG.with(|config| config.borrow().clone())
 }
-
+#[ic_cdk::query]
+fn get_config2() -> Config {
+    CONFIG.with(|config| config.borrow().clone())
+}
 ic_cdk::export_candid!();
 
