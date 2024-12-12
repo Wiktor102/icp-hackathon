85e898990441ef37835e06e1659d44e623414df0
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-12-12 10:05:20 +0100

    Basic Profile page layout

--- src/icp-hackathon-frontend/src/Profile/Profile.jsx
+++ src/icp-hackathon-frontend/src/Profile/Profile.jsx
@@ -1,6 +1,9 @@
 import { useIdentity } from "@nfid/identitykit/react";
 import { useNavigate } from "react-router";
 
+// components
+import ContactInfo from "../common/ContactInfo/ContactInfo";
+
 import "./Profile.scss";
 
 function Profile() {
@@ -13,8 +16,14 @@ function Profile() {
 	}
 
 	return (
-		<div>
-			<h1>Profile</h1>
+		<div className="profile-page">
+			<section className="profile-page__header">
+				<img src="https://picsum.photos/200" alt="zdjęcie profilowe" />
+				<div className="profile-page__header__panel-right">
+					<h1>Witaj, Janusz</h1>
+					<ContactInfo />
+				</div>
+			</section>
 		</div>
 	);
 }
