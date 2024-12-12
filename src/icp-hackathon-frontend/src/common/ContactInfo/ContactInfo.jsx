62c62a16dec83c55c5546b58688b277c93644329
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-12-12 14:41:56 +0100

    Update Profile component (grid view + data edit)

--- src/icp-hackathon-frontend/src/common/ContactInfo/ContactInfo.jsx
+++ src/icp-hackathon-frontend/src/common/ContactInfo/ContactInfo.jsx
@@ -1,32 +1,113 @@
+import { forwardRef, useRef, useState } from "react";
+import OutlinedButton from "../OutlinedButton/OutlinedButton";
+
 import "./ContactInfo.scss";
 
-function ContactInfo() {
+const Wrapper = forwardRef(({ condition, children, ...props }, ref) =>
+	condition ? (
+		<form {...props} ref={ref}>
+			{children}
+		</form>
+	) : (
+		<div {...props}>{children}</div>
+	)
+);
+
+function ContactInfo({ editButton = false }) {
+	const [editable, setEditable] = useState(false);
+	const formRef = useRef(null);
+
+	const validateUserData = () => {
+		const formData = new FormData(formRef.current);
+		const email = formData.get("e-mail");
+		const phone = formData.get("phone");
+
+		const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		const phonePattern = /^\d{9}$/;
+
+		if (!emailPattern.test(email)) {
+			alert("Nieprawidłowy adres e-mail");
+			return false;
+		}
+
+		if (!phonePattern.test(phone)) {
+			alert("Niepoprawny numer telefonu");
+			return false;
+		}
+
+		const name = formData.get("name");
+		const company = formData.get("company");
+
+		if (!name.includes(" ")) {
+			alert("Podaj pełne imię");
+			return false;
+		}
+
+		if (!company.trim()) {
+			alert("Nazwa firmy nie może być pusta");
+			return false;
+		}
+
+		return true;
+	};
+
+	const saveUserData = () => {
+		const formData = new FormData(formRef.current);
+	};
+
+	const onClick = () => {
+		setEditable(e => {
+			if (!e) return !e;
+			if (validateUserData()) return !e;
+			saveUserData();
+		});
+	};
+
 	return (
-		<section className="contact-info">
-			<h3>Dane kontaktowe</h3>
-			<div className="grid">
-				<div>
-					<i className="fas fa-building"></i>
-					<span className="label">Firma:</span>
-					<span>Ubraniex Sp ZOO</span>
-				</div>
-				<div>
-					<i className="fas fa-user"></i>
-					<span className="label">Imię i nazwisko:</span>
-					<span>Jan Kowalski</span>
-				</div>
-				<div>
-					<i className="fas fa-envelope"></i>
-					<span className="label">Adres e-mail:</span>
-					<span>abs@example.com</span>
-				</div>
-				<div>
-					<i className="fa fa-phone"></i>
-					<span className="label">Telefon:</span>
-					<span>123456789</span>
-				</div>
+		<div className="contact-info">
+			<div>
+				<h3>Dane kontaktowe</h3>
+				<Wrapper condition={editable} className="grid" ref={formRef}>
+					<div>
+						<i className="fas fa-building"></i>
+						<span className="label">Firma:</span>
+						{editable ? (
+							<input type="text" name="company" defaultValue="Ubraniex Sp ZOO" />
+						) : (
+							<span>Ubraniex Sp ZOO</span>
+						)}
+					</div>
+					<div>
+						<i className="fas fa-user"></i>
+						<span className="label">Imię i nazwisko:</span>
+						{editable ? (
+							<input type="text" name="name" defaultValue="Jan Kowalski" />
+						) : (
+							<span>Jan Kowalski</span>
+						)}
+					</div>
+					<div>
+						<i className="fas fa-envelope"></i>
+						<span className="label">Adres e-mail:</span>
+						{editable ? (
+							<input type="email" name="e-mail" defaultValue="abs@example.com" />
+						) : (
+							<span>abs@example.com</span>
+						)}
+					</div>
+					<div>
+						<i className="fa fa-phone"></i>
+						<span className="label">Telefon:</span>
+						{editable ? <input type="tel" name="phone" defaultValue="123456789" /> : <span>123456789</span>}
+					</div>
+				</Wrapper>
+				{editButton && (
+					<OutlinedButton onClick={onClick}>
+						<i className="fas fa-pencil"></i> {editable ? "Zapisz zmiany" : "Edytuj dane kontaktowe"}
+					</OutlinedButton>
+				)}
 			</div>
-		</section>
+		</div>
 	);
 }
 
