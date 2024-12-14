7db98f09f18b7f9aa174a2bf19c66967275e006b
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-12-14 13:35:10 +0100

    Tidy up frontend folder structure

--- src/icp-hackathon-frontend/src/App.jsx
+++ src/icp-hackathon-frontend/src/App.jsx
@@ -4,10 +4,10 @@ import { BrowserRouter, Route, Routes } from "react-router";
 // components
 import Home from "./Home/Home";
 import Header from "./Header/Header";
-import Profile from "./Profile/Profile";
-import AddListing from "./AddListing/AddListing";
-import Favorites from "./Favorites/Favorites.jsx";
-import ListingDetails from "./ListingDetails/ListingDetails";
+import Profile from "./pages/Profile/Profile.jsx";
+import Favorites from "./pages/Favorites/Favorites.jsx";
+import AddListing from "./pages/AddListing/AddListing.jsx";
+import ListingDetails from "./pages/ListingDetails/ListingDetails.jsx";
 
 import { IdentityKitAuthType, InternetIdentity } from "@nfid/identitykit";
 import { IdentityKitProvider } from "@nfid/identitykit/react";
