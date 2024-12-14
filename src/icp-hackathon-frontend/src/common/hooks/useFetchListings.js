e304697efcd775642a01210e106f4cb72380bcb6
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-12-14 11:41:38 +0100

    Process listings received from the backend

--- src/icp-hackathon-frontend/src/common/hooks/useFetchListings.js
+++ src/icp-hackathon-frontend/src/common/hooks/useFetchListings.js
@@ -2,6 +2,20 @@ import { useEffect, useRef } from "react";
 import { icp_hackathon_backend as backend } from "declarations/icp-hackathon-backend";
 import useStore from "../../store/store.js";
 
+function parseBackendListing(l) {
+	return {
+		...l,
+		owner: {
+			...l.owner,
+			phone: l.owner.phone_number,
+			company: l.owner.company_name,
+			company_name: undefined,
+			phone_number: undefined,
+			initialised: true
+		}
+	};
+}
+
 function useFetchListings() {
 	const listings = useStore(state => state.listings);
 	const addListings = useStore(state => state.addListings);
@@ -10,9 +24,9 @@ function useFetchListings() {
 	async function fetchListings() {
 		try {
 			const response = await backend.get_listings();
-			addListings(...response);
+			addListings(...response.map(parseBackendListing));
 		} catch (error) {
-			console.error(error);
+			console.error("(fetch listings) Backend error: " + error);
 		}
 	}
 
@@ -29,7 +43,7 @@ function useFetchCategoryListings() {
 	async function fetchListings(category) {
 		try {
 			const response = await backend.get_listings_by_category(category);
-			addListings(...response);
+			addListings(...response.map(parseBackendListing));
 		} catch (error) {
 			console.error(error);
 		}
