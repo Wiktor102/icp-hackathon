c80dfce1c5457354887c9feee894199625faa7f8
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-12-15 08:56:42 +0100

    Silly bugfix in <Empty/>

--- src/icp-hackathon-frontend/src/common/components/Empty/Empty.jsx
+++ src/icp-hackathon-frontend/src/common/components/Empty/Empty.jsx
@@ -4,7 +4,7 @@ function Empty({ icon = <i className="fas fa-folder-minus"></i>, children }) {
 	return (
 		<div className="empty-indicator">
 			{icon}
-			<p className="label">children</p>
+			<p className="label">{children}</p>
 		</div>
 	);
 }
